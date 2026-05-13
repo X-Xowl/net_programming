2ec4bf71d79d6fb36d6993457674a3e95d0318d3
X-Xowl <llxttxll@foxmail.com>
2026-05-13 21:57:57 +0800

    增强封装，修复bug

--- main.cpp
+++ main.cpp
@@ -42,54 +42,67 @@ public:
     {
         return fd;
     }
+    void bind(sockaddr_in& addr) const
+     {
+         if (::bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) == -1)
+         {
+             throw runtime_error("Failed to bind socket");
+         }
+     } //将socket绑定到指定的地址和端口上，如果绑定失败则抛出异常。
+    void listen(const int backlog) const
+    {
+        if (::listen(fd, backlog) == -1)
+        {
+            throw runtime_error("Failed to listen on socket");
+        }
+    }
+    void setsockopt(int level,int optname, int& opt) const
+     {
+         if (::setsockopt(fd, level, optname, &opt, sizeof(opt)) == -1)
+         {
+             throw runtime_error("Failed to set socket options");
+         }
+     }
+    ssize_t recv(char* buffer,int len,int flags) const
+    {
+         ssize_t n = ::recv(fd, buffer, len, flags);
+         return n;
+    }
+    bool send_all(const char* data, const size_t len) const
+    {
+         size_t sent = 0;
+         while (sent < len)
+         {
+             ssize_t n = ::send(fd, data + sent, len - sent, 0);
+             if (n <= 0)
+             {
+                 return false;
+             }
+             sent += n;
+         }
+         return true;
+    }
 private:
     int fd;
 };
-inline bool send_all(const int fd, const char* data, const size_t len)
-{
-    size_t sent = 0;
-    while (sent < len)
-    {
-        ssize_t n = send(fd,data + sent,len - sent,0);
-        if (n <= 0)
-        {
-            return false;
-        }
-        sent += n;
-    }
-    return true;
-}
 
 int main()
 {
-    Socket listen_socket(AF_INET,SOCK_STREAM,0);
-    int listen_fd = listen_socket.get_fd();
+    const Socket listen_socket(AF_INET,SOCK_STREAM,0);
     int opt = 1;
-    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
-    {
-        perror("setsockopt");
-        return -1;
-    }
+    listen_socket.setsockopt(SOL_SOCKET,SO_REUSEADDR,opt);
     sockaddr_in server_addr{};
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8080);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(listen_fd,reinterpret_cast<sockaddr*>(&server_addr),sizeof(server_addr)) == -1)
-    {
-        perror("bind");
-        return -1;
-    }
-    if (listen(listen_fd, SOMAXCONN) == -1)
-    {
-        perror("listen");
-        return -1;
-    }
+    listen_socket.bind(server_addr);
+    listen_socket.listen(SOMAXCONN);
     // ReSharper disable once CppDFAEndlessLoop
     while (true)
     {
         sockaddr_in client_addr{};
         socklen_t client_len = sizeof(client_addr);
-        int client_fd = accept(listen_fd,reinterpret_cast<sockaddr*>(&client_addr),&client_len);
+        const int client_fd = accept(listen_socket.get_fd(),reinterpret_cast<sockaddr*>(&client_addr),&client_len);
         Socket client_socket(client_fd);
         array<char,INET_ADDRSTRLEN> client_ip{};
         inet_ntop(
@@ -102,11 +115,15 @@ int main()
         array<char,4096> buffer{};
         while (true)
         {
-            ssize_t n = recv(client_fd, buffer.data(), sizeof(buffer), 0);
+            ssize_t n = client_socket.recv(buffer.data(),buffer.size(),0);
             if (n>0)
             {
                 cout << "Received from client: " << string_view(buffer.data(),n) << endl;
-                if (!send_all(client_fd,buffer.data(),n))
+                if (client_socket.send_all(buffer.data(),n))
+                {
+                    cout << "Sent data back to client" << endl;
+                }
+                else
                 {
                     cerr << "Failed to send data to client" << endl;
                     break;
